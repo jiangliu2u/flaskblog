8b824cef12a72ea3e708490db70c9dd6f475af38
jiangliu2u <jiangliu2u@gmail.com>
2018-01-26 21:55:10 +0800

    index

--- flaskr.py
+++ flaskr.py
@@ -3,13 +3,20 @@ import pymongo
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+app.config.from_envvar('FLASKR_SETTINGS', silent = True)
 
 
 def connect_db():
     """Connects to the specific database."""
     connection = pymongo.MongoClient('127.0.0.1:27017')
     return connection
+
+
+@app.route('/')
+def index():
+    cn = connect_db()
+    diary = cn.flask.blog.find()
+    return render_template('index.html', diaries=diary)
 
 
 if __name__ == "__main__":
